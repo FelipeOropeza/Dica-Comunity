da16f5c1c0cc3ceb565de701649bbba93c5148d0
FelipeOropeza <felipe2006.co@gmail.com>
2024-09-18 22:32:25 -0300

    feat: login do usuario cadastrado na API, retornado um token

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/form/Form";
+import axios from "axios";
 
-import "./Login.css"
+import "./Login.css";
+
+const apiUrl = import.meta.env.VITE_API;
 
 function Login() {
-  const handleLogin = ({ email, password }) => {
-    // Lógica de Login (por exemplo, chamar a API de login)
-    console.log("Login com:", { email, password });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async ({ email, senha }) => {
+    try {
+      const response = await axios.post(`${apiUrl}auth/login`, {
+        email,
+        senha,
+      });
+      console.log(response.data);
+      setErrorMessage("");
+    } catch (error) {
+      const message = error.response
+        ? error.response.data
+        : "Erro desconhecido. Tente novamente mais tarde.";
+      setErrorMessage(message);
+    }
   };
 
   return (
     <div className="login">
       <h2>Login</h2>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
       <Form onSubmit={handleLogin} isLoginMode={true} />
     </div>
   );
-};
+}
 
 export default Login;
+
