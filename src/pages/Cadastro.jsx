0a15530d9e86fe6597e3bf20de7f02e06cea1217
FelipeOropeza <felipe2006.co@gmail.com>
2024-09-18 22:31:58 -0300

    feat: cadastro de usuario na base de dados da API

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/form/Form";
+import axios from "axios";
 
 import "./Cadastro.css";
 
+const apiUrl = import.meta.env.VITE_API;
+
 function Cadastro() {
-  const handleCadastro = ({ email, password, nome }) => {
-    // Lógica de Cadastro (por exemplo, chamar a API de cadastro)
-    console.log("Cadastro com:", { email, password, nome });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleCadastro = async ({ email, senha, nome }) => {
+    try {
+      const response = await axios.post(`${apiUrl}usuario`, {
+        nome,
+        email,
+        senha,
+      });
+      console.log(response.data);
+      setErrorMessage("");
+    } catch (error) {
+      const message = error.response
+        ? error.response.data
+        : "Erro desconhecido. Tente novamente mais tarde.";
+      setErrorMessage(message);
+      return { error: message };
+    }
   };
 
   return (
     <div className="login-cadastro">
       <h2>Cadastro</h2>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
       <Form onSubmit={handleCadastro} isLoginMode={false} />
     </div>
   );
