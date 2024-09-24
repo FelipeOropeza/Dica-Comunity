50d84e7818c36939ff9a31c2339130f6962f2229
FelipeOropeza <felipe2006.co@gmail.com>
2024-09-23 21:41:40 -0300

    feat: mundança no design

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,18 @@
 import "./Card.css";
 
-function Card(pros) {
-  const titulo = pros.titulo;
-  const body = pros.body;
+function Card({ titulo, body, likes, comments }) {
   return (
     <div id="card">
       <h2>{titulo}</h2>
       <p>{body}</p>
+      <div className="card-stats">
+        <div className="likes">
+          <span>{likes} curtidas</span>
+        </div>
+        <div className="comments">
+          <span>{comments} comentários</span>
+        </div>
+      </div>
     </div>
   );
 }
